ceaf0fae6cedb50ee47fe584d4a140110f75b802
Owoeye1321 <owoeye1321@gmail.com>
2022-09-24 13:36:14 -0700

    new commit

--- src/components/Contact/Forms.js
+++ src/components/Contact/Forms.js
@@ -1,119 +1,136 @@
-import React from 'react';
-//import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from 'react';
-//import axios from 'axios'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
+import axios from 'axios'
 
 function Forms() {
-//     const [data, setData ] = useState({
-//         fname:'',
-//         email:'',
-//         subject:'',
-//         message:''
-//     })
+  const [data, setData] = useState({
+    fname: '',
+    email: '',
+    subject: '',
+    message: '',
+  })
 
-//     const handle = (e)=>{
-//         const newData = {...data}
-//         newData[e.target.id] = e.target.value
-//         setData(newData)
-//       //  console.log(data)
-//     }
-    
-//     const onSubmit = async(e)=>{      
-//         e.preventDefault()
-//         const result = await axios.post('https://futa-hostels-10467.herokuapp.com/dashboard',{data})
-//    if(result.data == "success"){
-//            alert("Email has been sent successfully to C-rentals official")
-//         window.location.assign("https://futa-hostel-rentals-c3bf0b.netlify.app/")
-//        }else{
-//         console.log('Unable to send email')
-//         //    alert('An error has occured')
-//         //    alert('An error has occured')
-        
-//     }
-//     }  
-    return (
-        <>
-            <section className="contact-section">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="contact--info-area">
-                                <h3>Get in touch</h3>
-                                <p>Looking for help? Fill the form and start a new adventure.</p>
-                                <div className="single-info">
-                                    <h5>Headquaters</h5>
-                                    <p>
-                                        <i className="fal fa-home"></i>
-                                        Kwara State University
-                                        <br /> Malete
-                                    </p>
-                                </div>
-                                <div className="single-info">
-                                    <h5>Phone</h5>
-                                    <p>
-                                        <i className="fal fa-phone"></i>
-                                       <a href='tel:+2349164787591'>(+234) 916 478 7591</a>
-                                        <br /> 
-                                        </p>
-                                        <p>
-                                        <i className="fal fa-phone"></i>
-                                        <a href='tel:+2349153464158'>(+234) 915 346 4158</a>
-                                        </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-8">
-                            <div className="contact-form">
-                                <h4>Let’s Connect</h4>
-                                <p>Send us a message to connect with you.</p>
-                                <form className="row" onSubmit = {(e)=>submit(e)} >
-                                    <div className="col-md-6">
-                                        <input required type="text" onChange = {(e)=>handle(e)}   id="fname" placeholder="Name" />
-                                    </div>
-                                    <div className="col-md-6">
-                                        <input required 
-                                            onChange = {(e)=>handle(e)}  
-                                            type="email"
-                                            id="email"
-                                            placeholder="Email Address"
-                                        />
-                                    </div>
-                                    <div className="col-md-12">
-                                        <input required onChange = {(e)=>handle(e)}   type="text" id="subject" placeholder="Subject" />
-                                    </div>
-                                    <div className="col-md-12">
-                                        <textarea
-                                            required        
-                                            onChange = {(e)=>handle(e)}  
-                                            id="message"
-                                            placeholder="How can we help?"
-                                        ></textarea>
-                                    </div>
-                                    <div className="col-md-6">
-                                        <div className="condition-check">
-                                            
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6 text-right">
-                                        <input type="submit" value="SendMessage" onClick={(e)=>{
-                                            onSubmit(e)
-                                        }}/>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+  const handle = (e) => {
+    const newData = { ...data }
+    newData[e.target.id] = e.target.value
+    setData(newData)
+    //  console.log(data)
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    //       const result = await axios.post('https://futa-hostels-10467.herokuapp.com/dashboard',{data})
+    //  if(result.data == "success"){
+    //          alert("Email has been sent successfully to C-rentals official")
+    //       window.location.assign("https://futa-hostel-rentals-c3bf0b.netlify.app/")
+    //      }else{
+    //       console.log('Unable to send email')
+    //       //    alert('An error has occured')
+    //       //    alert('An error has occured')
+
+    //   }
+  }
+  return (
+    <>
+      <section className="contact-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="contact--info-area">
+                <h3>Get in touch</h3>
+                <p>
+                  Looking for help? Fill the form and start a new adventure.
+                </p>
+                <div className="single-info">
+                  <h5>Headquaters</h5>
+                  <p>
+                    <i className="fal fa-home"></i>
+                    Kwara State University
+                    <br /> Malete
+                  </p>
                 </div>
-            </section>
-
-            <div className="bisylms-map">
-                <iframe
-                    title="map"
-                    src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=en&amp;coord=39.966528,-75.158284&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=p&amp;z=16&amp;iwloc=B&amp;output=embed"
-                ></iframe>
+                <div className="single-info">
+                  <h5>Phone</h5>
+                  <p>
+                    <i className="fal fa-phone"></i>
+                    <a href="tel:+2349164787591">(+234) 916 478 7591</a>
+                    <br />
+                  </p>
+                  <p>
+                    <i className="fal fa-phone"></i>
+                    <a href="tel:+2349153464158">(+234) 915 346 4158</a>
+                  </p>
+                </div>
+              </div>
             </div>
-        </>
-    );
+            <div className="col-md-8">
+              <div className="contact-form">
+                <h4>Let’s Connect</h4>
+                <p>Send us a message to connect with you.</p>
+                <form className="row" onSubmit={(e) => onSubmit(e)}>
+                  <div className="col-md-6">
+                    <input
+                      required
+                      type="text"
+                      onChange={(e) => handle(e)}
+                      id="fname"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <input
+                      required
+                      onChange={(e) => handle(e)}
+                      type="email"
+                      id="email"
+                      placeholder="Email Address"
+                    />
+                  </div>
+                  <div className="col-md-12">
+                    <input
+                      required
+                      onChange={(e) => handle(e)}
+                      type="text"
+                      id="subject"
+                      placeholder="Subject"
+                    />
+                  </div>
+                  <div className="col-md-12">
+                    <textarea
+                      required
+                      onChange={(e) => handle(e)}
+                      id="message"
+                      placeholder="How can we help?"
+                    ></textarea>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="condition-check"></div>
+                  </div>
+                  <div className="col-md-6 text-right">
+                    <input
+                      type="submit"
+                      value="SendMessage"
+                      onClick={(e) => {
+                        onSubmit(e)
+                      }}
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="bisylms-map">
+        <iframe
+          title="map"
+          src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=en&amp;coord=39.966528,-75.158284&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=p&amp;z=16&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+      </div>
+    </>
+  )
 }
 
-export default Forms;
+export default Forms
--- src/components/Dashboard/admin.js
+++ src/components/Dashboard/admin.js
@@ -1,61 +1,61 @@
-import { React, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import logo from '../../assets/images/how-it-work-thumb.png';
-//import axios from "axios";
-
+import { React, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/images/how-it-work-thumb.png'
+import axios from 'axios'
 
 function Admin() {
-
-  const [userDetails, setUserDetails] = useState([]);
+  const [userDetails, setUserDetails] = useState([])
   const [addHostelDetails, setAddHostelDetails] = useState({
-    hostel_name:'',
-    address:'',
-    socialAmenities:'',
-    username:sessionStorage.getItem('username')
-  });
+    name: '',
+    address: '',
+    details: '',
+  })
   const [hostelPicture, setHostelPicture] = useState('')
 
-    //// performing some dangerous operation round around the spagetti environment
-    const [pickFirst , setPickFirst] = useState('block')
-    const [pickSecond , setPickSecond] = useState('none')
-    const [pickThird, setPickThird] = useState('block')
-    const [pickFourth, setPickFourth] = useState('none')
+  //// performing some dangerous operation round around the spagetti environment
+  const [pickFirst, setPickFirst] = useState('block')
+  const [pickSecond, setPickSecond] = useState('none')
+  const [pickThird, setPickThird] = useState('block')
+  const [pickFourth, setPickFourth] = useState('none')
 
-  const LogOut = async()=>{
-    const logUserOut = await axios.get('https://futa-hostels-10467.herokuapp.com/logOut')
-    if(logUserOut.data === "success"){
-      sessionStorage.clear()
-      window.location.assign('https://futa-hostel-rentals-c3bf0b.netlify.app/login')
-    }
+  const LogOut = async () => {
+    // const logUserOut = await axios.get(
+    //   'https://futa-hostels-10467.herokuapp.com/logOut',
+    // )
+    // if (logUserOut.data === 'success') {
+    //   sessionStorage.clear()
+    //   window.location.assign(
+    //     'https://futa-hostel-rentals-c3bf0b.netlify.app/login',
+    //   )
+    // }
   }
-  const handleSubmit = async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
-//     console.log(hostelPicture)
-//     const addForm = new FormData()
-//     addForm.append('data', JSON.stringify(addHostelDetails))
-//     addForm.append('file',hostelPicture)
-//    // console.log(addForm)
-//     const checkingFormUpdates = await axios.post('https://futa-hostels-10467.herokuapp.com/addHostelDetails',addForm)
-//     if(checkingFormUpdates.data === 'success'){ 
-//       console.log('Hostel information saved successfully')
-//       alert('Hostel information saved successfully')
-//     }
-  } 
+    //     console.log(hostelPicture)
+    //     const addForm = new FormData()
+    //     addForm.append('data', JSON.stringify(addHostelDetails))
+    //     addForm.append('file',hostelPicture)
+    //    // console.log(addForm)
+    //     const checkingFormUpdates = await axios.post('https://futa-hostels-10467.herokuapp.com/addHostelDetails',addForm)
+    //     if(checkingFormUpdates.data === 'success'){
+    //       console.log('Hostel information saved successfully')
+    //       alert('Hostel information saved successfully')
+    //     }
+  }
 
-    const handleChange = (e)=>{
-      const addNewData = { ...addHostelDetails }
-      addNewData[e.target.id]= e.target.value
-      setAddHostelDetails(addNewData)
-      //console.log(addHostelDetails)
-    }
-    const addHostelPicture = (e)=>{
-      let name = e.target.files[0]
-      setHostelPicture(name)
-  //    console.log(hostelPicture)
+  const handleChange = (e) => {
+    const addNewData = { ...addHostelDetails }
+    addNewData[e.target.id] = e.target.value
+    setAddHostelDetails(addNewData)
+    //console.log(addHostelDetails)
+  }
+  const addHostelPicture = (e) => {
+    let name = e.target.files[0]
+    setHostelPicture(name)
+    //    console.log(hostelPicture)
+  }
 
-    }
-
-  useEffect(()=>{
+  useEffect(() => {
     // alert('hello there i am trying to control the width of the screen')
     // alert(window.width)
     // const response = async ()=>{
@@ -64,7 +64,6 @@ function Admin() {
     //   //  / console.log(check.data)
     // }
     // response()
-
     //  const fetchAll = async () =>{
     //   await axios.post('https://futa-hostels-10467.herokuapp.com/read',{username:sessionStorage.getItem('username')}).then((res)=>{
     //     setUserDetails(res.data)
@@ -72,69 +71,74 @@ function Admin() {
     //   }).catch((err)=>{
     //     console.log('An error has occured' , err)
     //   })
-
     //  }
     //  fetchAll()
-    
-},[])
+  }, [])
   return (
     <>
       <section
         className="appie-hero-area"
-        style={{ marginTop: "-100px", marginBottom: "-80px" }}
+        style={{ marginTop: '-100px', marginBottom: '-80px' }}
       >
         <div className="row">
           <div className="col-sm-12 col-md-5 col-lg-4 p-4">
-            <div style={{ width: "100%" }}>
+            <div style={{ width: '100%' }}>
               <div
                 className="min-vh-100 d-flex flex-column opacity-mask"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+                style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
               >
                 <div
                   className="m-5 bg-white px-5 py-3"
                   style={{
-                    width: "400px",
-                    alignSelf: "center",
-                    boxShadow: "5px 5px 45px 5px lightgrey",
-                    alignItems: "right",
+                    width: '400px',
+                    alignSelf: 'center',
+                    boxShadow: '5px 5px 45px 5px lightgrey',
+                    alignItems: 'right',
                   }}
                 >
-                    <h4 className="m-2"></h4>
-                  <form 
-                  onSubmit={(e)=>{handleSubmit(e)}}
+                  <h4 className="m-2"></h4>
+                  <form
+                    onSubmit={(e) => {
+                      handleSubmit(e)
+                    }}
                   >
                     <div className="form-group">
-                      <input 
-                      required
-                        onChange={(e)=>{ handleChange(e)}}
+                      <input
+                        required
+                        onChange={(e) => {
+                          handleChange(e)
+                        }}
                         className="form-control"
                         type="text"
-                        placeholder="Hostel Name"
-
-                        id="hostel_name"
+                        placeholder="Gadget Name"
+                        id="name"
                       />
                     </div>
                     <div className="form-group">
                       <input
-                      required
-                       onChange={(e)=>{ handleChange(e)}}
+                        required
+                        onChange={(e) => {
+                          handleChange(e)
+                        }}
                         className="form-control"
                         type="text"
-                        placeholder=" Address"
+                        placeholder="Address"
                         id="address"
                       />
                     </div>
                     <div className="form-group">
                       <textarea
-                       onChange={(e)=>{ handleChange(e)}}
+                        onChange={(e) => {
+                          handleChange(e)
+                        }}
                         className="form-control"
                         required
-                        placeholder="Social Amenities"
+                        placeholder="Gadget Issues"
                         style={{
-                          height: "150px",
-                          borderRadius: "10px",
+                          height: '150px',
+                          borderRadius: '10px',
                         }}
-                        id="socialAmenities"
+                        id="details"
                       />
                     </div>
 
@@ -143,14 +147,14 @@ function Admin() {
                     </div>
                     <div className="form-group">
                       <input
-                      required
-                      onChange={(e)=>{
-                        addHostelPicture(e)
-                      }}
+                        required
+                        onChange={(e) => {
+                          addHostelPicture(e)
+                        }}
                         type="file"
                         id="file"
                         className="form-control"
-                        name = "file"
+                        name="file"
                       />
                     </div>
                     <div className="form-group">
@@ -158,7 +162,7 @@ function Admin() {
                         type="submit"
                         value="submit"
                         className="form-control bg-primary"
-                        style={{ color: "white" }}
+                        style={{ color: 'white' }}
                       />
                     </div>
                   </form>
@@ -167,12 +171,12 @@ function Admin() {
             </div>
           </div>
           <div className="col-sm-12 col-md-8 col-lg-8">
-                      <h1>Details are posted here</h1>
+            <h1>Details are posted here</h1>
           </div>
         </div>
       </section>
     </>
-  );
+  )
 }
 
-export default Admin;
+export default Admin
